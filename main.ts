54e2572f02999a095eeca3cc3d9ee6932e180cbe
MoReDAKKA1 <25jbrunelle@aos92.org>
2025-02-14 11:02:50 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+let mySprite = sprites.create(assets.image`Army detailed stickfigure`, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+assets.animation`you caught me doing what    question mark`,
+500,
+false
+)
