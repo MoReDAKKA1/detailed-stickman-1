f2d150c10fb30bc45086555b6fe86f338060719c
MoReDAKKA1 <25jbrunelle@aos92.org>
2025-02-25 09:24:29 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-let mySprite = sprites.create(assets.image`Army detailed stickfigure`, SpriteKind.Player)
-animation.runImageAnimation(
-mySprite,
-assets.animation`you caught me doing what    question mark`,
-500,
-false
-)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`you caught me doing what    question mark`,
+    500,
+    false
+    )
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`Army detailed stickfigure`, SpriteKind.Player)
+scene.setBackgroundColor(8)
